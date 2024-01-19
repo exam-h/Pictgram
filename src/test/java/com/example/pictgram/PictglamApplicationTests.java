218b58c6ead4775d96d1e89b12ff846b5c097159
Taro <taro@example.com>
2024-01-19 17:04:40 +0900

    first commit

---
+++ src/test/java/com/example/pictgram/PictglamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pictgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictglamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
